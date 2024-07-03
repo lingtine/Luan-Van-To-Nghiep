57ad12e3f22993f3b762fa90eb4e834d03261f25
lingtine <lingtine@gmail.com>
2024-07-03 19:35:41 +0700

    update

--- src/pages/client/home/Banner.tsx
+++ src/pages/client/home/Banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Carousel } from "@material-tailwind/react";
+import { Button, Carousel, IconButton } from "@material-tailwind/react";
 import { useGetProductCarouselQuery } from "redux/api/catalog/product";
 import { Link } from "react-router-dom";
 interface BannerProps {}
@@ -37,7 +37,59 @@ const Banner: React.FC<BannerProps> = () => {
     });
 
     return (
-      <Carousel autoplay={true} autoplayDelay={4000} loop>
+      <Carousel
+        autoplay={true}
+        autoplayDelay={4000}
+        loop
+        prevArrow={({ handlePrev }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handlePrev}
+            className="!absolute top-2/4 left-4 -translate-y-2/4 text-black"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+          </IconButton>
+        )}
+        nextArrow={({ handleNext }) => (
+          <IconButton
+            variant="text"
+            color="white"
+            size="lg"
+            onClick={handleNext}
+            className="!absolute top-2/4 !right-4 -translate-y-2/4 text-black"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </IconButton>
+        )}
+      >
         {content}
       </Carousel>
     );
